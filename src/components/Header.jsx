9709800a8b065c34d9c8a2cf22b71d57b6731ab6
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-03-31 23:45:51 +0530

    Test Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../../public/duhookLogo.png"
 const Header = () => {
   return (
     <div className="flex bg-black text-white items-center font-[poppins]">
-      <img src={logo} className="w-16" alt="" />
+      
       <span>DUHook</span>
     </div>
   )
