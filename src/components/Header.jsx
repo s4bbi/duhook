d543a680f758fba26f96567e05abddab2299cb01
Priya <priyakandpal79@gmail.com>
2024-04-09 13:10:06 +0530

    header-done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,13 +9,13 @@ const Header = () => {
         <div className="flex bg-black text-white items-center font-[poppins] px-5 gap-x-24 hustify-between">
           <div className="flex ">
           <img src={logo} alt="Logo" className="w-16"/>
-          <span className="font-rM font-bold pt-5">DuHook</span>
+          <span className="font-rM font-bold pt-5 cursor-pointer">DuHook</span>
           </div>
-          <img src="https://play-lh.googleusercontent.com/L3JSdLbbJF2WSYm1DGDeiQSId1ftJKRrVX-Ne2cSjdzAzTLlHcuNW1LWKiWM8ttZbPc" className="sm:w-24 w-14 "></img>
+          <img src="https://play-lh.googleusercontent.com/L3JSdLbbJF2WSYm1DGDeiQSId1ftJKRrVX-Ne2cSjdzAzTLlHcuNW1LWKiWM8ttZbPc" className="sm:w-24 w-14 cursor-pointer"></img>
           
           <div className="flex ">
-          <FaSistrix className="text-sky-500 w-20 h-6 font-medium"/>
-          <FaAlignJustify className="text-sky-500 w-12 h-6"/>
+          <FaSistrix className="text-sky-500 w-20 h-6 font-medium cursor-pointer"/>
+          <FaAlignJustify className="text-sky-500 w-12 h-6 cursor-pointer"/>
 
           </div>
           <div>
