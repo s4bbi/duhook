c42fa8e456293d2e185065ce89ebd037ea861d51
Priya <priyakandpal79@gmail.com>
2024-04-09 13:07:11 +0530

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import logo from "../../public/duhookLogo.png"
+import { FaSistrix } from "react-icons/fa";
+import { FaAlignJustify } from "react-icons/fa";
 
 const Header = () => {
   return (
     <div>
       <div>
-        <div className="flex bg-black text-white items-center font-[poppins] px-5">
-          <img src={logo} alt="Logo" className="w-20"/>
-          <span className="font-rM">DUHook</span>
+        <div className="flex bg-black text-white items-center font-[poppins] px-5 gap-x-24 hustify-between">
+          <div className="flex ">
+          <img src={logo} alt="Logo" className="w-16"/>
+          <span className="font-rM font-bold pt-5">DuHook</span>
+          </div>
+          <img src="https://play-lh.googleusercontent.com/L3JSdLbbJF2WSYm1DGDeiQSId1ftJKRrVX-Ne2cSjdzAzTLlHcuNW1LWKiWM8ttZbPc" className="sm:w-24 w-14 "></img>
+          
+          <div className="flex ">
+          <FaSistrix className="text-sky-500 w-20 h-6 font-medium"/>
+          <FaAlignJustify className="text-sky-500 w-12 h-6"/>
+
+          </div>
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
